7187bc70a9529a96f9fb553aed5672d735c4c24a
C4rlosJeronimo <107066970+C4rlosJeronimo@users.noreply.github.com>
2024-09-28 15:22:16 -0300

    Update Bruxo.java

--- EX_herança/src/Bruxo.java
+++ EX_herança/src/Bruxo.java
@@ -1,11 +1,8 @@
 class Bruxo extends Personagem {
-    // Atributo específico
     private int carisma;
 
-    // Construtor
     public Bruxo(String nome, int nivel, int vida, int mana, int carisma) {
         super(nome, nivel, vida, mana);
         this.carisma = carisma;
     }
-
 }
