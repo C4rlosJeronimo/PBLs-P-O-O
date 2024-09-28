16052cdfe67ebec1a9d391274d540574eb3967a3
C4rlosJeronimo <107066970+C4rlosJeronimo@users.noreply.github.com>
2024-09-28 15:22:36 -0300

    Guerreiro.java

--- EX_herança/src/Guerreiro.java
+++ EX_herança/src/Guerreiro.java
@@ -1,11 +1,8 @@
 class Guerreiro extends Personagem {
-    // Atributo específico
     private int forca;
 
-    // Construtor
     public Guerreiro(String nome, int nivel, int vida, int mana, int forca) {
         super(nome, nivel, vida, mana);
         this.forca = forca;
     }
-
 }
