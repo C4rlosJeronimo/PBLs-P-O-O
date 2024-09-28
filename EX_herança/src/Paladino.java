8f807bb1ecd04e18d63049b89b16419e08c51a81
C4rlosJeronimo <107066970+C4rlosJeronimo@users.noreply.github.com>
2024-09-28 15:23:00 -0300

    Update Paladino.java

--- EX_herança/src/Paladino.java
+++ EX_herança/src/Paladino.java
@@ -1,11 +1,8 @@
 class Paladino extends Personagem {
-    // Atributo específico
     private int fe;
 
-    // Construtor
     public Paladino(String nome, int nivel, int vida, int mana, int fe) {
         super(nome, nivel, vida, mana);
         this.fe = fe;
     }
-
 }
